c82664be3ee0152e87e914ddbf988c6f9585c179
Ian Boyle <irboyle21@gmail.com>
2021-10-15 13:49:07 -0400

    adds post route and create action. add http file to perfom post route/action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,27 @@
 
 
 class ProductsController < ApplicationController
-  def get_products
-    products = Product.all
-    render json: {products: products.as_json}
-  end
-  def get_first_product
-    product = Product.first
-    render json: {products: product.as_json}
-  end
-  def get_last_product
-    product = Product.last
-    render json: {products: product.as_json}
-  end
-  def get_second_product
-    product = Product.second
-    render json: {products: product.as_json}
-  end
-  def get_any_product
-    input_value = params[:product_id].to_i
-    product = Product.find_by(id: input_value)
-    render json: {products: product.as_json}
-  end
+  # def get_products
+  #   products = Product.all
+  #   render json: {products: products.as_json}
+  # end
+  # def get_first_product
+  #   product = Product.first
+  #   render json: {products: product.as_json}
+  # end
+  # def get_last_product
+  #   product = Product.last
+  #   render json: {products: product.as_json}
+  # end
+  # def get_second_product
+  #   product = Product.second
+  #   render json: {products: product.as_json}
+  # end
+  # def get_any_product
+  #   input_value = params[:product_id].to_i
+  #   product = Product.find_by(id: input_value)
+  #   render json: {products: product.as_json}
+  # end
   def index
     products = Product.all
     render json: {products: products.as_json}
@@ -30,5 +30,14 @@ class ProductsController < ApplicationController
     product_id = params["id"]
     product = Product.find_by(id: product_id)
     render json: {product: product.as_json}
+  end
+
+  def create
+    # product = Product.new(name: FFaker::Product.product_name, price: FFaker::Number.number, description: FFaker::Product.product)
+    # product.save
+
+    product = Product.new(name: params["name"], price: params["price"], description: params["description"])
+    product.save
+    render json: product.as_json
   end
 end
